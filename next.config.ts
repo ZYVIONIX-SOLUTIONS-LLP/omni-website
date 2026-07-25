d68b7640df6fce2f199bea81c99961256cbd4f0d
akshithjeevan <akshithjeevan2000@gmail.com>
2026-07-25 18:28:42 +0530

    fix: set images.unoptimized to true to prevent server 503 image optimization errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
